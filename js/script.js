262797d0e2a6cd0f3849896558d25ea22c7a3b16
rodriiContreras <rodrigitocontre@hotmail.com>
2021-06-28 21:19:56 -0300

    cambios en carrito, login , etc

--- js/script.js
+++ js/script.js
@@ -17,12 +17,12 @@ window.sr = ScrollReveal();
    
   });
   
-  ScrollReveal().reveal('.vermas', {
+  ScrollReveal().reveal('#vermas', {
     duration:4000,
   });
 
 
-  ScrollReveal().reveal('.bienvenidos',{
+  ScrollReveal().reveal('#bienvenidos',{
     duration:2000,
     origin:'right',
     distance: '-800px'
@@ -78,11 +78,32 @@ almacenador.push(Email);
 almacenador.push(Contrasenia);
 console.log(JSON.stringify(almacenador));
 
-document.getElementById("registro").style.visibility=("hidden");
-document.getElementById("formulariologueo").style.visibility=("visible")
-document.getElementById("formulariologueo").style.zIndex=("+4000")
-document.getElementById("overlay").style.backgroundColor=("rgba(0, 0, 0, 0.747)")
-document.getElementById("overlay").style.zIndex=("+1000")
+if(document.getElementById("registro_nombreusuario").value == length > 1 && document.getElementById("registro_email").value == length > 1 && document.getElementById("registro_contrasena").value == length > 1 ){
+  document.getElementById("register_parrafo").innerText=("Por lo menos debe ingresar una letra en los campos")
+  document.getElementById("register_parrafo").style.color=("red")
+  console.log("mal")
+//document.getElementById("formulariologueo").style.visibility=("hidden")
+//document.getElementById("formulariologueo").style.zIndex=("+4000")
+//document.getElementById("overlay").style.backgroundColor=("rgba(0, 0, 0, 0.747)")
+//document.getElementById("overlay").style.zIndex=("+1000")
+}
+else if(document.getElementById("registro_email").value == length > 1 ){
+  document.getElementById("register_parrafo").innerText=("Por lo menos debe ingresar una letra en el campo de Email")
+  document.getElementById("register_parrafo").style.color=("red")
+}
+else if (document.getElementById("registro_contrasena").value == length > 1 ){
+  document.getElementById("register_parrafo").innerText=("Por lo menos debe ingresar una letra en el campo de Contraseña")
+  document.getElementById("register_parrafo").style.color=("red")
+}
+else{
+  document.getElementById("registro").style.visibility=("hidden");
+  document.getElementById("formulariologueo").style.visibility=("visible")
+  document.getElementById("formulariologueo").style.zIndex=("+4000")
+  document.getElementById("overlay").style.backgroundColor=("rgba(0, 0, 0, 0.747)")
+  document.getElementById("overlay").style.zIndex=("+1000")
+  
+}
+
 
 sessionStorage.setItem("registrousuario",NombreUsuario);
 sessionStorage.setItem("registropassword",Contrasenia);
@@ -97,14 +118,14 @@ sessionStorage.setItem("registroemail",Email);
 var loguear = document.getElementById("formulariologueo_aceptar");
 loguear.addEventListener("click", Iniciarsesion);
 
-function Iniciarsesion(){//Tomo los dos valores principales y los mando a un almacenador aparte tambien, y genero un if para que se asegure que sean iguales los datos del registro y del logueo
+function Iniciarsesion(){//Tomo los dos valores principales y los mand a un almacenaodor aparte tambien, y genero un if para que se asegure que sean iguales los datos del registro y del logueo
 let nombreDeUsuario = document.getElementById("formulariologueo_nombreusuario").value;
 let contra = document.getElementById("formulariologueo_contrasena").value;
 almacenadorlog.push(nombreDeUsuario);
 almacenadorlog.push(contra);
 console.log(JSON.stringify(almacenadorlog));
 
-if(document.getElementById("formulariologueo_nombreusuario").value !== document.getElementById("registro_nombreusuario").value){//Esta linea de codigo toma los valores de "Nombre de usuario" que se ponen en el registro y los compara con el que se puso en logueo.  
+if( document.getElementById("formulariologueo_nombreusuario").value !== document.getElementById("registro_nombreusuario").value){//Esta linea de codigo toma los valores de "Nombre de usuario" que se ponen en el registro y los compara con el que se puso en logueo.  
   document.getElementById("parrafologueo").style.color=("red");
   document.getElementById("parrafologueo").innerText=("Nombre de usuario y/o contraseña incorrecta")
   console.log("mal")
@@ -117,6 +138,11 @@ else if (document.getElementById("registro_contrasena").value !== document.getEl
   document.getElementById("parrafologueo").innerText=("Nombre de usuario y/o contraseña incorrecta")
   console.log("mal")
 
+}
+else if (document.getElementById("formulariologueo_nombreusuario").value == length > 1){
+  document.getElementById("parrafologueo").style.color=("red");
+  document.getElementById("parrafologueo").innerText=("Por lo menos debe ingresar una letra en los campos")
+  console.log("mal")
 }
 else{
   document.getElementById("parrafologueo").style.color=("green");
@@ -157,17 +183,18 @@ logueo.addEventListener("mouseover", prueba);
 
 // -----------------------------Login ----------------------------
 class Juegos {
-    constructor (nombre, precio) {
+    constructor (nombre, precio,imagen) {
         this.nombre  = nombre;
         this.precio  = parseFloat(precio);
-    }
+        this.imagen = imagen    
+      }
 }
-const juego1 = new Juegos("Metro Redux ", "2000");
+const juego1 = new Juegos("Metro Redux ", "2000")
 console.log(juego1);
 const juego2 = new Juegos("Resident Evil 3", "2500");
 console.log( juego2);
 const juego3 = new Juegos("Detroit", "2700");
-console.log(juego3);
+console.log(juego3);  
 const juego4 = new Juegos("Uncharted 4", "2500");
 console.log(juego4);
 const juego5 = new Juegos("Call of duty Modern Warfare", "2000");
@@ -183,7 +210,7 @@ console.log(juego9);
 const juego10 = new Juegos("Ghost Of Tsushima", "3500");
 console.log(juego10);
 const blanco = " "
-const productosJuegos = []
+let productosJuegos = []
 var mostrarCartel = document.getElementById("cartelcarrito");
 console.log(mostrarCartel);
 var textoCartel = document.getElementById("parrafocarrito");
@@ -191,9 +218,7 @@ console.log(textoCartel.innerText);
 function AgregarACarrito(){
 productosJuegos.push(new Juegos(juego1.nombre , juego1.precio * 1.21));
 console.log (JSON.stringify("Se ha agregado el juego= " + juego1.nombre + " al precio de " + juego1.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="metroredux" src="../imagenes/metro.png">
-<h2 id="juego1">${juego1.nombre + juego1.precio * 1.21}</h2>
-`);
+ $("#productoscarrito").append(`<img id="metroredux" src="../imagenes/metro.png"><h2 id="juego1">${juego1.nombre + juego1.precio * 1.21}</h2>`);
 
 if(productosJuegos.push){
   textoCartel = document.getElementById("parrafocarrito").textContent= "Se Ha agregado a carrito " + juego1.nombre;
@@ -205,7 +230,7 @@ if(productosJuegos.push){
    
     setTimeout(function borrarelemento(){
       mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-    },1000);
+    },600);
     
 }
 
@@ -214,7 +239,7 @@ function AgregarACarrito2(){
  productosJuegos.push(new Juegos(juego2.nombre, juego2.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego2.nombre + " al precio de " + juego2.precio * 1.21 + " (Incluyendo IVA)"));
 
-$("#productoscarrito").append(`<img id="residentevil" src="../imagenes/residentevil.jpg">
+ $("#productoscarrito").append(`<img id="residentevil" src="../imagenes/residentevil.jpg">
 <h2 id="juego2">${juego2.nombre + blanco +  juego2.precio}</h2>
 `);
 
@@ -227,12 +252,12 @@ if(productosJuegos.push){
   
     setTimeout(function borrarelemento(){
       mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-    }, 1000);
+    }, 600);
   }
 function AgregarACarrito3(){
 productosJuegos.push(new Juegos(juego3.nombre, juego3.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego3.nombre + " al precio de " + juego3.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_detroit" src="../imagenes/detroit.jpg"">
+ $("#productoscarrito").append(`<img id="carrito_detroit" src="../imagenes/detroit.jpg"">
 <h2 id="juego3">${juego3.nombre + blanco +juego3.precio * 1.21}</h2>
 `);
 if(productosJuegos.push){
@@ -245,16 +270,16 @@ if(productosJuegos.push){
  
   setTimeout(function borrarelemento(){
     mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-  }, 1000);
+  }, 600);
 }
 
 }
 function AgregarACarrito4(){
-productosJuegos.push(new Juegos(juego4.nombre, juego4.precio ));
-console.log(JSON.stringify("Se ha agregado el juego= " + juego4.nombre + " al precio de " + juego4.precio + " (Incluyendo IVA)"));
+productosJuegos.push(new Juegos(juego4.nombre, juego4.precio * 1.21));
+console.log(JSON.stringify("Se ha agregado el juego= " + juego4.nombre + " al precio de " + juego4.precio * 1.21 + " (Incluyendo IVA)"));
 
-$("#productoscarrito").append(`<img id="carrito_uncharted" src="../imagenes/uncharted4.png">
-<h2 id="juego4">${juego4.nombre + juego4.precio}</h2>
+ $("#productoscarrito").append(`<img id="carrito_uncharted" src="../imagenes/uncharted4.png">
+<h2 id="juego4">${juego4.nombre +blanco + juego4.precio * 1.21}</h2>
 `);
 
 if(productosJuegos.push){
@@ -266,14 +291,14 @@ if(productosJuegos.push){
   
   setTimeout(function borrarelemento(){
     mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-  }, 1000);
+  }, 600);
 }
 
 
 function AgregarACarrito5(){
 productosJuegos.push(new Juegos(juego5.nombre, juego5.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego5.nombre + " al precio de " + juego5.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_cod" src="../imagenes/callofduty.jpg">
+ $("#productoscarrito").append(`<img id="carrito_cod" src="../imagenes/callofduty.jpg">
 <h2 id="juego5">${juego5.nombre +blanco+ juego5.precio}</h2>
 `);
 if(productosJuegos.push){
@@ -285,13 +310,13 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("blue
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 }
 
 function AgregarACarrito6(){
 productosJuegos.push(new Juegos(juego6.nombre, juego6.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego6.nombre + " al precio de " + juego6.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_tlou" src="../imagenes/tlou.jpg">
+ $("#productoscarrito").append(`<img id="carrito_tlou" src="../imagenes/tlou.jpg">
 <h2 id="juego6">${juego6.nombre +blanco+ juego6.precio * 1.21}</h2>
 `);
 if(productosJuegos.push){
@@ -303,13 +328,13 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("blac
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 }
 }
 function AgregarACarrito7(){
 productosJuegos.push(new Juegos(juego7.nombre, juego7.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego7.nombre + " al precio de " + juego7.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_reddead" src="../imagenes/reddead.jpg">
+ $("#productoscarrito").append(`<img id="carrito_reddead" src="../imagenes/reddead.jpg">
 <h2 id="juego7">${juego7.nombre +blanco + juego7.precio * 1.21}</h2>
 `);
 if(productosJuegos.push){
@@ -321,14 +346,14 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("blue
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 }
 
 }
 function AgregarACarrito8(){
 productosJuegos.push(new Juegos(juego8.nombre, juego8.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego8.nombre + " al precio de " + juego8.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_sekiro" src="../imagenes/sekiro.jpg">
+ $("#productoscarrito").append(`<img id="carrito_sekiro" src="../imagenes/sekiro.jpg">
 <h2 id="juego8">${juego8.nombre + blanco +juego8.precio * 1.21}</h2>
 `);
 if(productosJuegos.push){
@@ -340,14 +365,14 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("gree
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 }
 
 }
 function AgregarACarrito9(){
 productosJuegos.push(new Juegos(juego9.nombre, juego9.precio * 1.21));
 console.log(JSON.stringify("Se ha agregado el juego= " + juego9.nombre + " al precio de " + juego9.precio * 1.21 + " (Incluyendo IVA)"));
-$("#productoscarrito").append(`<img id="carrito_starwars" src="../imagenes/starwars.jpg">
+  $("#productoscarrito").append(`<img id="carrito_starwars" src="../imagenes/starwars.jpg">
 <h2 id="juego9">${juego9.nombre + blanco +juego9.precio * 1.21}</h2>
 `);
 if(productosJuegos.push){
@@ -359,7 +384,7 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("blac
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 
  }
 }
@@ -378,7 +403,7 @@ mostrarCartel = document.getElementById("cartelcarrito").style.background=("blue
 
 setTimeout(function borrarelemento(){
   mostrarCartel = document.getElementById("cartelcarrito").style.visibility=("hidden")
-}, 1000);
+}, 600);
 
   }
 }
@@ -397,23 +422,30 @@ else (innerWidth < 1300)
 )
 
 $(document).ready(function carrito(){
-  $("#jquerycarrito").append(`<div id="nombreproducto"><h1>Aqui esta su carrito:</h1></div>
-  <div id="productoscarrito"></div>`);
+  $("#jquerycarrito").append(`<div id="nombreproducto"><h1>Su Carrito<i id="jquerycarrito_carrito" class="fas fa-shopping-cart"></i>:</h1></div>
+  <div id="productoscarrito"></div>
+  <button type="Button" id="vaciarcarrito" class="btn btn-success" onclick="Vaciarcarrito()">Vaciar Carrito</button>
+  <button type="Button" id="cerrarcarrito" class="btn btn-success" onclick="Cerrarcarrito()">Cerrar Carrito</button>`);
+ 
 })
-//const TodosLosJuegos = [{}]
-//----------------------
+$("#vaciarcarrito").click(function(){
+  $("#productoscarrito").empty();
+  console.log("este evento esta sucediendo")
+});
+function Vaciarcarrito(){
+  $("#productoscarrito").empty();
+  console.log("este evento sucede")
+}
 
-function Lisstacarrito(){
-   document.getElementById("jquerycarrito").style.visibility=("visible");
-  console.log(productosJuegos);
-  for(const producto of productosJuegos){
-    $("#productoscarrito").append(`<h2>${productosJuegos.forEach(juego =>{
-    })}</h2>`)
-    } }
-    Lisstacarrito();
     function Listacarrito(){
+     
       document.getElementById("jquerycarrito").style.visibility=("visible");
-     console.log(productosJuegos);
-
     }
-//----------------------
+
+
+    function Cerrarcarrito(){
+      document.getElementById("jquerycarrito").style.visibility=("hidden");
+    }
+    //----------------------
+
+
